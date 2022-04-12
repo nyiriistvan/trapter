3772dacb46e70d15ef4c3abe19c6284d96dc8b1b
nyiriistvan <nyiri.istvan19@gmail.com>
2022-04-12 10:37:29 +0200

    trapter1

---
+++ app.java/src/App.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+/*
+* File: trapter.java
+* Author: Nyiri István
+* Copyright: 2022, Nyiri István
+* Group: Szoft I N 
+* Date: 2022-02-22
+* Github: https://github.com/nyiriistvan
+* Licenc: GNU GPL
+*/
+
+class trapter {
+public static void main(String[] args) {
+   System.out.println(" Trapéz területe\n Nyiri István \n 2022-02-22 Szoft I N");
+
+  
+       
+}
+}
+}
+
