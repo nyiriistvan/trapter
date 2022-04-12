67222759bb4cfc05949355af1676289fe724b34f
nyiriistvan <nyiri.istvan19@gmail.com>
2022-04-12 10:44:28 +0200

    kész a feladat és müködik

--- app.java/src/App.java
+++ app.java/src/App.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+
 /*
 * File: trapter.java
 * Author: Nyiri István
@@ -12,19 +13,24 @@ import java.util.Scanner;
 class trapter {
 public static void main(String[] args) {
    System.out.println(" Trapéz területe\n Nyiri István \n 2022-02-22 Szoft I N");
-   
-   System.out.print("aoldal:"); 
-   int aoldal = scan.nextInt();  
 
-   System.out.print("boldal:"); 
-   int boldal = scan.nextInt();
-
-   System.out.print("coldal:"); 
-   int coldal = scan.nextInt();
-
-   System.out.print("doldal:"); 
-   int doldal = scan.nextInt();
-  
+   try (Scanner scan = new Scanner(System.in)) {
+    System.out.print("aoldal:"); 
+       int aoldal = scan.nextInt();  
+    
+       System.out.print("boldal:"); 
+       int boldal = scan.nextInt();
+    
+       System.out.print("coldal:"); 
+       int coldal = scan.nextInt();
+    
+       System.out.print("doldal:"); 
+       int doldal = scan.nextInt();
+    
+       double terület = ((aoldal) + boldal) / 4* (aoldal - coldal)
+       * Math.sqrt((aoldal+boldal-coldal+doldal)*(aoldal-boldal-coldal+doldal)*
+       (aoldal+boldal-coldal-doldal)*(-aoldal+boldal+coldal+doldal));
+       System.out.println("A trapéz területe:" + terület);
        
 }
 }
