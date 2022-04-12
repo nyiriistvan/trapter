63f975a2fb6213ea77e611b3e6ae874f3baf9b45
nyiriistvan <nyiri.istvan19@gmail.com>
2022-04-12 10:39:56 +0200

    trapter2

--- app.java/src/App.java
+++ app.java/src/App.java
@@ -12,7 +12,18 @@ import java.util.Scanner;
 class trapter {
 public static void main(String[] args) {
    System.out.println(" Trapéz területe\n Nyiri István \n 2022-02-22 Szoft I N");
+   
+   System.out.print("aoldal:"); 
+   int aoldal = scan.nextInt();  
 
+   System.out.print("boldal:"); 
+   int boldal = scan.nextInt();
+
+   System.out.print("coldal:"); 
+   int coldal = scan.nextInt();
+
+   System.out.print("doldal:"); 
+   int doldal = scan.nextInt();
   
        
 }
